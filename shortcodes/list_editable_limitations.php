7c9a2a16b2a1e72e51a4d31ab0e999b8c1820dea
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2025-04-07 11:15:04 +0200

    refactor listing of editable limitations

--- shortcodes/list_editable_limitations.php
+++ shortcodes/list_editable_limitations.php
@@ -2,82 +2,45 @@
 
 require_once get_stylesheet_directory() . '/inc/database.php';
 require_once get_stylesheet_directory() . '/constants.php';
+require_once get_stylesheet_directory() . '/shortcodes/list_editable_items.php';
 
 add_shortcode('list_editable_limitations', 'list_editable_limitations');
 
 function list_editable_limitations(): string {
-    $limitations = get_all(FUNCTIONAL_LIMITATION_TABLE, 'name');
-    $output = '';
-
-    if (!empty($limitations)) {
-        $output .= '<table>';
-        $output .= '<tr><th>Funktionseinschränkung</th><th>Aktionen</th></tr>';
-
-        foreach ($limitations as $limitation) {
-            $output .= '<tr>';
-            $output .= '<td>' . $limitation->name . '</td>';
-            $output .= '<td>';
-            $output .= '<a href="' . esc_url(site_url('/funktionseinschraenkung-bearbeiten?id=' . $limitation->id)) . '">';
-            $output .= '<button>Bearbeiten</button>';
-            $output .= '</a>';
-            $output .= '<button class="delete-limitation" data-id="' . esc_attr($limitation->id) . '">Löschen</button>';
-            $output .= '</td>';
-            $output .= '</tr>';
-        }
-
-        $output .= '</table>';
-
-        $output .= '<div id="delete-dialogue" style="display: none">' .
-            '<div class="modal-content" id="modal-content"></div>' .
-            '</div>';
-
-    } else {
-        $output .= '<p>Keine Funktionseinschränkungen gefunden.</p>';
-    }
-
-    return $output;
+    return list_editable_items(
+            FUNCTIONAL_LIMITATION_TABLE,
+        'name',
+        'Funktionseinschränkung',
+        'display_by_name',
+        'funktionseinschraenkung-bearbeiten',
+        'delete-limitation',
+        'Funktionseinschränkungen'
+    );
 }
 
 function delete_limitation_script(): void {
-    ?>
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            document.querySelectorAll(".delete-limitation").forEach(button => {
-                button.addEventListener("click" , function (event) {
-                    event.preventDefault();
-                    let limitationId = this.getAttribute("data-id");
-                    let dialogue = document.getElementById('delete-dialogue');
-                    let modalContent = document.getElementById('modal-content');
+    generate_delete_function(
+            'deleteLimitation',
+            'delete_limitation'
+    );
 
-                    modalContent.innerHTML = "<span class='close' onclick='closeDialogue()'>&times;</span>" +
-                        "<p>Bist du sicher, dass du diese Funktionseinschränkung löschen möchtest?</p>" +
-                        "<button onclick='deleteLimitation(" + limitationId + ")'>Ja</button> " +
-                        "<button onclick='closeDialogue()'>Abbrechen</button>";
+    generate_modal_content_script(
+            'generateLimitationModal',
+            'Funktionseinschränkung',
+            'deleteLimitation'
+    );
 
-                    dialogue.style.display = "block";
-                });
-            });
-        });
-
-        function deleteLimitation(limitationId) {
-            fetch('<?php echo admin_url("admin-ajax.php")?>', {
-                method: "POST",
-                headers: {"Content-Type": "application/x-www-form-urlencoded"},
-                body: "action=delete_limitation&limitation_id=" + limitationId
-            })
-                .then(() => {
-                    this.location.reload();
-                })
-        }
-    </script>
-    <?php
+    delete_empty_item_script(
+            'delete-limitation',
+            'generateLimitationModal'
+    );
 }
 
 add_action('wp_footer', 'delete_limitation_script');
 
 function delete_limitation(): void {
     global $wpdb;
-    $limitation_id = intval($_POST['limitation_id']);
+    $limitation_id = intval($_POST['item_id']);
     $wpdb->delete(FUNCTIONAL_LIMITATION_TABLE, ['id' => $limitation_id]);
     wp_send_json(['success' => true]);
 }
