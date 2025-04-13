c2339edd757cdb50f73311041cc9b9bda6474d3b
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2025-04-13 11:35:00 +0200

    make all headings focusable

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,9 @@
 function closeDialogue() {
     document.getElementById("delete-dialogue").style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(heading => {
+        heading.setAttribute("tabindex", 0);
+    });
+})
