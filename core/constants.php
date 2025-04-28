468aa50d1310307b855d3dfc91c34e204a7ad951
Tom Frederik Leimbrock <tom.frederik.leimbrock@webmail.thm.de>
2025-04-29 00:33:21 +0200

    use snake case for table names

--- core/constants.php
+++ core/constants.php
@@ -4,19 +4,19 @@ if (!defined("ABSPATH")) {
 }
 
 /* table names */
-const AIDS_WITH_DISABILITY_TABLE = "aidsWithDisability";
-const AIDS_WITH_LIMITATION_TABLE = "aidsWithLimitation";
-const PRODUCT_CATEGORY_TABLE = "productCategory";
+const AIDS_WITH_DISABILITY_TABLE = "aids_with_disability";
+const AIDS_WITH_LIMITATION_TABLE = "aids_with_limitation";
+const PRODUCT_CATEGORY_TABLE = "product_category";
 const AVAILABILITY_TABLE = "availability";
-const CATEGORY_OF_PRODUCT_TABLE = "categoryOfProduct";
+const CATEGORY_OF_PRODUCT_TABLE = "category_of_product";
 const DISABILITY_TABLE = "disability";
-const DISABILITY_CATEGORY_TABLE = "disabilityCategory";
-const FUNCTIONAL_LIMITATION_TABLE = "functionalLimitation";
+const DISABILITY_CATEGORY_TABLE = "disability_category";
+const FUNCTIONAL_LIMITATION_TABLE = "functional_limitation";
 const PRODUCT_TABLE = "product";
 const UNIVERSITY_TABLE = "university";
-const ADDITIONAL_LINK_TABLE = "additionalLink";
-const LINK_FOR_AID_TABLE = "linkForAid";
-const LINK_FOR_DISABILITY_TABLE = "linkForDisability";
+const ADDITIONAL_LINK_TABLE = "additional_link";
+const LINK_FOR_AID_TABLE = "link_for_aid";
+const LINK_FOR_DISABILITY_TABLE = "link_for_disability";
 const CONSULTANT_TABLE = "consultant";
 
 /* other constants */
